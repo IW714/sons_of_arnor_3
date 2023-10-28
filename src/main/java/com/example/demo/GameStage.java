5182f60d74802e6686f0869e2df54e41542b5533
Ian Bracken <ibracken@ad.unc.edu>
2023-10-28 13:31:32 -0400

    a

--- src/main/java/com/example/demo/GameStage.java
+++ src/main/java/com/example/demo/GameStage.java
@@ -4,7 +4,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.Scanner;
 
+//This is where we are putting in the actual code for the game
 public class GameStage extends Stage {
     Label x = new Label("Second Stage");
     VBox y = new VBox();
@@ -13,5 +15,7 @@ public class GameStage extends Stage {
         y.getChildren().add(x);
         this.setScene(new Scene(y, 600, 400));
         this.show();
+        Scanner UI = new Scanner(System.in);
+        System.out.println("Welcome to Sons of Arnor, a choose your own adventure game. What is your name?");
     }
 }
