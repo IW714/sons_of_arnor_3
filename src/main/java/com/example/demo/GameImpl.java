3620783540a6948fb2183163842c93c7f174097d
Ian Bracken <ibracken@ad.unc.edu>
2023-10-28 16:07:49 -0400

    a

--- src/main/java/com/example/demo/GameImpl.java
+++ src/main/java/com/example/demo/GameImpl.java
@@ -41,16 +41,47 @@ public class GameImpl implements Game {
     public static Game createGame() {
         Scanner myObj = new Scanner(System.in);
 //        GameStage.
-        println("Welcome to Sons of Arnor, a choose your own adventure game. What is your name?");
+        System.out.println("Welcome to Sons of Arnor, a choose your own adventure game. What is your name?");
         String name = myObj.nextLine();
-        println("Hello " + name + "! In Sons of Arnor, each player will have 3 different stats: Strength, Health, " +
+        System.out.println("Hello " + name + "! In Sons of Arnor, each player will have 3 different stats: Strength, Health, " +
                 "and Mana.");
-        System.out.println("How much Strength would you like: 1-10");
-        int strength = myObj.nextInt();
-        System.out.println("How much Health would you like: 1-10");
-        int health = myObj.nextInt();
-        System.out.println("How much Mana would you like: 1-10");
-        int mana = myObj.nextInt();
+        int strength = 0;
+        int health = 0;
+        int mana = 0;
+        boolean valid = true;
+        int totalAttributes = 0;
+        System.out.println("How much Strength would you like: 1-10(You may have 15 attributes split between the stats)");
+        while(valid) {
+            strength = myObj.nextInt();
+            if(strength < 1 || strength > 10) {
+                System.out.println("Strength must be between 1-10");
+            }
+            else {
+                valid = false;
+            }
+        }
+        valid = true;
+        totalAttributes += strength;
+
+        System.out.println("How much Health would you like: 1-10(You may have 15 attributes split between the stats)");
+        while(valid) {
+            health = myObj.nextInt();
+            if(health < 1 || health > 10) {
+                System.out.println("Health must be between 1-10");
+            }
+            else if (totalAttributes + health > 14) {
+                System.out.println("Too many attributes have been selected, pick a lower number");
+            }
+            else {
+                valid = false;
+            }
+        }
+        valid = true;
+        totalAttributes += health;
+
+
+        mana = 15 - totalAttributes;
+        System.out.println("Your remaining points will be spent on mana. You have " + mana + "mana.");
 
         PlayerSheet playerSheet = new PlayerSheetImpl(strength, health, mana, new ArrayList<>());
         Inventory inventory = new InventoryImpl(new ArrayList<>(), 0);
